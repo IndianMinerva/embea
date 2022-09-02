0adf7780c1ddb127dda8b2a7b2cb3f78e7f09f9b
Arun Kumar <arun@Penguin.local>
2022-09-02 17:36:08 +0200

    Initial Commit

---
+++ src/main/java/com/factory/embea/EmbeaApplication.java
@@ -0,0 +1,13 @@
+package com.factory.embea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbeaApplication.class, args);
+	}
+
+}
