6ff48f9e89026559b72079178307ab4d79c23fce
Arun Kumar <arun@Penguin.local>
2022-09-07 09:28:11 +0200

    Adds mongoTemplate config

--- src/main/java/com/factory/embea/config/SimpleMongoConfig.java
+++ src/main/java/com/factory/embea/config/SimpleMongoConfig.java
@@ -4,30 +4,30 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
+@Slf4j
 public class SimpleMongoConfig {
-    /*@Value("${spring.data.mongodb.host}")
+    @Value("${spring.data.mongodb.host}")
     private String mongoHost;
 
     @Value("${spring.data.mongodb.port}")
     private String mongoPort;
 
     @Value("${spring.data.mongodb.database}")
-    private String mongoDatabase;*/
+    private String mongoDatabase;
 
     @Bean
     public MongoClient mongo() {
-        System.out.println("***************************************************************");
-        //System.out.println("mongodb://" + mongoHost + ":" + mongoPort + "/" + mongoDatabase);
-        System.out.println("***************************************************************");
+        String mongoUrl = "mongodb://" + mongoHost + ":" + mongoPort + "/" + mongoDatabase;
+        log.info("Mongo connection string : {}" + mongoUrl);
 
-        //ConnectionString connectionString = new ConnectionString("mongodb://" + mongoHost + ":" + mongoPort + "/" + mongoDatabase);
-        ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/policy");
+        ConnectionString connectionString = new ConnectionString(mongoUrl);
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .build();
@@ -37,7 +37,6 @@ public class SimpleMongoConfig {
 
     @Bean
     public MongoTemplate mongoTemplate() throws Exception {
-        //return new MongoTemplate(mongo(), mongoDatabase);
-        return new MongoTemplate(mongo(), "policy");
+        return new MongoTemplate(mongo(), mongoDatabase);
     }
 }
